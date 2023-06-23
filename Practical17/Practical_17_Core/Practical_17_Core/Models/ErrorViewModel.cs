b612ebbcd313cbcd1cece831755383ff9a9d50b6
jainam.b <jainam.b@simformsolutions.com>
2023-06-23 13:08:43 +0530

    Completed Practical17
    Implemented Repository Pattern with Dependency Injection for performing CRUD operations with the Student table

---
+++ Practical17/Practical_17_Core/Practical_17_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Practical_17_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? ErrorMessage { get; set; } = "Internal server error";
+        public string? ErrorDetails { get; set; }
+    }
+}
---
+++ Practical17/Practical_17_Core/Practical_17_Core/Models/Student.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace Practical_17_Core.Models
+{
+    public class Student
+    {
+        [Display(Name = "Student Id")]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+
+        [Display(Name = "First Name")]
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        [Column(TypeName = "VARCHAR(50)")]
+        public string? FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        [Column(TypeName = "VARCHAR(50)")]
+        public string? LastName { get; set; }
+
+        [Display(Name = "Email")]
+        [Required]
+        [StringLength(200, MinimumLength = 10)]
+        [EmailAddress]
+        [Column(TypeName = "VARCHAR(50)")]
+        public string? Email { get; set; }
+
+        [Display(Name = "Mobile Number")]
+        [Required]
+        [StringLength(10, MinimumLength = 10)]
+        [Column(TypeName = "VARCHAR(10)")]
+        [Phone]
+        public string? MobileNumber { get; set; }
+
+        [Display(Name = "Address")]
+        [MaxLength(500)]
+        [Column(TypeName = "VARCHAR(500)")]
+        public string? Address { get; set; }
+    }
+}
