f77d16a5f7eef792f5da1d7dda1f28657ce65f56
jainam.b <jainam.b@simformsolutions.com>
2023-06-23 12:01:40 +0530

    Completed Practical20
    Implemented global exception handling using middleware, Implemented a generic repository for database operations using the Unit of Work pattern.
    Implemented asynchronous functions.

---
+++ Practical20/Practical20/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Practical20.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+        public DbSet<Students> Students { get; set; }
+    }
+}
